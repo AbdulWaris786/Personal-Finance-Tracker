cf9fcc06e2dbe878b1c599681c3fbe71caf72967
abdul waris <warismuthuparamba@gmail.com>
2024-08-23 22:48:16 +0530

    signup page design completed

--- src/app/Authantication/auth.module.ts
+++ src/app/Authantication/auth.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from "@angular/core";
 import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
 
 @NgModule({
     declarations:[
-        LoginComponent
+        LoginComponent,
+        SignupComponent
     ],
     exports:[
-        LoginComponent
+        LoginComponent,
+        SignupComponent
     ],
     imports:[
 
